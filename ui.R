2cfebb57005b5fb7e42bbe45aa712b8564c3daca
Paul R Staab <staab@bio.lmu.de>
2014-08-22 17:55:49 +0200

    add a few links

--- ui.R
+++ ui.R
@@ -18,7 +18,9 @@ shinyUI(
                         'information.')),
       
       h4("Info"),
-      p('Jaatha is a parameter estimation method for',
+      p(a('Jaatha', 
+          href='http://cran.r-project.org/web/packages/jaatha/index.html'),
+        'is a parameter estimation method for',
         'Evolutionary Biology, available via CRAN package',
         em('jaatha.'), 
         'Given a model of evolution, it estimates',
@@ -31,7 +33,15 @@ shinyUI(
         'and the relative estimation error and runtime are',
         'calulated.'),
       p('Above, you can select if you want to plot the estimation errors',
-        '(accuary) or the runtime for the different models and versions.'),      
+        '(accuary) or the runtime for the different models and versions.'),
+      p(a('Source Code', 
+          href='https://github.com/paulstaab/jaatha-benchmark-explorer'),
+        ' - ',
+        a('Benchmark Script', 
+          href='https://github.com/paulstaab/jaatha-benchmark'),
+        ' - ',
+        a('Raw Data', 
+          href='https://github.com/paulstaab/jaatha-benchmark/blob/master/data_processed/acc_and_runtime.Rda?raw=true')),
       
       width = 3
     ),
